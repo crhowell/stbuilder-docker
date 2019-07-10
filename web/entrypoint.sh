2c0791ec721c293d25c021eb92659dfd02a81c41
Chris Howell <crhowell@users.noreply.github.com>
2019-07-09 19:13:08 -0700

    update entry point
    add makemigrations command to solve fixture issue?

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 echo "Running Django Entrypoint commands"
 python manage.py collectstatic --no-input
+python manage.py makemigrations
 python manage.py migrate
 python manage.py loaddata initial_data
 exec gunicorn social_team_builder.wsgi:application --bind 0.0.0.0:8081 --workers 3
